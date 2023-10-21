d7b787eb6040d80d975cbb28fbd31b0e91383946
DaniBacheva <y.bacheva@abv.bg>
2023-10-21 17:22:55 +0300

    edit offer

--- src/controllers/offerController.js
+++ src/controllers/offerController.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 const offerManager = require('../manager/offerManager');
-const { getErrorMessage } = require('../util/errorHandler'); 
+const { getErrorMessage } = require('../util/errorHandler');
 
 router.get("/catalog", async (req, res) => {
     try {
         const offers = await offerManager.getAll().lean();
-        console.log({ offers })
+       // console.log({ offers })
 
         res.render("offers/catalog", { offers });
     }
@@ -47,16 +47,60 @@ router.get('/:offerId/details', async (req, res) => {
 
     try {
         const offer = await offerManager.getOne(offerId).lean();
-        const {user} = req;
-        const {owner} = offer;
-        const isOwner = user?._id===owner.toString();
-        console.log(isOwner)
+        const { user } = req;
+        const { owner } = offer;
+        const isOwner = user?._id === owner.toString();
+      //  console.log(isOwner)
         res.render('offers/details', { offer, isOwner });
     }
     catch (error) {
         res.status(400).render('offers/details', { error: getErrorMessage(error) });
     }
 });
+
+router.get('/:offerId/edit', async (req, res) => {
+    const offerId = req.params.offerId;
+
+    try {
+        const offer = await offerManager.getOne(offerId).lean();
+        res.render('offers/edit', { offer })
+    }
+    catch (error) {
+        res.status(400).render('offers/details', { error: getErrorMessage(error) });
+    }
+});
+
+router.post('/:offerId/edit', async(req, res) => {
+    const offerId = req.params.offerId;
+    const { name,
+        type,
+        damages,
+        image,
+        description,
+        production,
+        exploitation,
+        price
+    } = req.body;
+    
+    console.log(req.body);
+    const payload = {  name,
+        type,
+        damages,
+        image,
+        description,
+        production,
+        exploitation,
+        price
+    }
+
+    try {
+        await offerManager.update( offerId, payload);
+        res.redirect(`/offers/${offerId}/details`);
+    }
+    catch(error) {
+        res.status(400).render(`offers/${offerId}/details`, { error: getErrorMessage(error) });
+    }
+})
 
 
 module.exports = router;
--- src/manager/offerManager.js
+++ src/manager/offerManager.js
@@ -5,3 +5,5 @@ exports.create = (offerData) => Offer.create(offerData);
 exports.getAll = ()=> Offer.find();
 
 exports.getOne = (offerId)=> Offer.findById(offerId);
+
+exports.update = (offerId, offerData)=> Offer.findByIdAndUpdate(offerId, offerData);
